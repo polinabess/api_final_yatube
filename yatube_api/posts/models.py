c3d358b940b79fd1c69a79f3dd4b26a2ca7f98ea
Polina Bessogonova <girls_kiss@mail.ru>
2023-01-23 02:39:48 +0300

    created new models

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -24,3 +24,28 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follow'
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+
+    class Meta:
+        unique_together = ('user', 'following')
